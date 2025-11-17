091e7e95e6023327f3d3846465727963887736a5
Michal Kuchar <mikokuchar@gmail.com>
2025-11-17 14:44:05 +0100

    Change examples to reproduce paper results

---
+++ src/config/config_llama_7b.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass
+
+from torch import nn
+from torch.nn import modules
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.models.llama.modeling_llama import LlamaAttention, LlamaRMSNorm
+
+from config.config_protocol import ConfigProtocol, MHAProjection
+from infra.utils.dep_graph_utils.custom_pruners import OperationPruner, RMSNormPruner
+
+@dataclass
+class Config(ConfigProtocol):
+    DEVICE = 'cuda'
+    MODEL = AutoModelForCausalLM.from_pretrained('meta-llama/Llama-2-7b-hf', device_map='auto')
+    TOKENIZER = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-hf')
+    DUMMY_INPUT = MODEL.dummy_inputs['input_ids']
+    IMPORTANCES_SAVE_PATH = None
+    TARGET_SPARSITIES = []
+    PRUNED_MODEL_SAVE_DIR = None
+    EVALUATE = True
+    EVAL_RESULTS_PATH = './llama-7b-pruned.json'
+    DEP_GRAPH_ARGS = {
+        'output_transform': lambda output: output.logits,
+        'customized_pruners': {
+            LlamaRMSNorm: RMSNormPruner(),
+            nn.SiLU: OperationPruner(),
+        }
+    }
+    TRANSFORM_EXCLUSION_KEYWORDS = set([
+        'lm_head',
+        'fc',
+        'classifier'
+    ])
+    BASE_ATTENTION_TYPES = set([
+        LlamaAttention,
+    ])
+    MHA_PROJECTION_NAME_MAPPING = {
+        MHAProjection.Q: 'q_proj',
+        MHAProjection.K: 'k_proj',
+        MHAProjection.V: 'v_proj',
+        MHAProjection.O: 'o_proj'
+    }
+    BASE_TRANSFORM_TYPES = set([
+        modules.Linear,
+        modules.conv._ConvNd
+    ])
+    BASE_ACT_TYPES = set([
+        nn.ELU,
+        nn.Hardshrink,
+        nn.Hardsigmoid,
+        nn.Hardtanh,
+        nn.Hardswish,
+        nn.LeakyReLU,
+        nn.LogSigmoid,
+        nn.PReLU,
+        nn.ReLU,
+        nn.ReLU6,
+        nn.RReLU,
+        nn.SELU,
+        nn.CELU,
+        nn.GELU,
+        nn.Sigmoid,
+        nn.SiLU,
+        nn.Mish,
+        nn.Softplus,
+        nn.Softshrink,
+        nn.Softsign,
+        nn.Tanh,
+        nn.Tanhshrink,
+        nn.Threshold,
+        nn.GLU,
+        nn.Softmin,
+        nn.Softmax,
+        nn.Softmax2d,
+        nn.LogSoftmax,
+        nn.AdaptiveLogSoftmaxWithLoss,
+    ])
+    BASE_NORM_TYPES = set([
+        nn.BatchNorm1d,
+        nn.BatchNorm2d,
+        nn.BatchNorm3d,
+        nn.LazyBatchNorm1d,
+        nn.LazyBatchNorm2d,
+        nn.LazyBatchNorm3d,
+        nn.GroupNorm,
+        nn.SyncBatchNorm,
+        nn.InstanceNorm1d,
+        nn.InstanceNorm2d,
+        nn.InstanceNorm3d,
+        nn.LazyInstanceNorm1d,
+        nn.LazyInstanceNorm2d,
+        nn.LazyInstanceNorm3d,
+        nn.LayerNorm,
+        nn.LocalResponseNorm,
+        nn.RMSNorm,
+        LlamaRMSNorm
+    ])
---
+++ src/reprod_llama_7b_fine.py
@@ -0,0 +1,80 @@
+import json
+
+from config.config_llama_7b import Config as cfg
+from infra.evaluator import evaluate_pruned
+from infra.passes.identity_patching import IdentityPatcher
+from infra.utils.model_utils import ModelUtils
+
+from infra.pruning_tree_types.transform_pruning_tree import TransformPruningTree
+from infra.pruning_tree_types.attention_pruning_tree import AttentionPruningTreeGenerator
+
+model_utils = None
+if cfg.MODEL is not None:
+    model_utils = ModelUtils(cfg)
+    print("Model loaded")
+
+with open('./angular-7b-fine.json') as file:
+    importances = json.load(file)
+
+mlp_importances = []
+attn_importances = []
+
+for imp, name in sorted(importances):
+    if 'attn' in name:
+        attn_importances.append((imp, name))
+    else:
+        mlp_importances.append((imp, name))
+
+# Mapping from % parameter reduction to number of iterations
+FIGURE_POINTS = {
+    "0%": 0,
+    "6%": 5,
+    "12%": 12,
+    "18%": 18,
+    "24%": 25,
+    "30%": 35,
+    "36%": 45,
+    "42%": 52,
+    "48%": 56,
+    "54%": 63,
+}
+
+mlp_idx = 0
+attn_idx = 0
+for iter in range(FIGURE_POINTS["0%"]):
+    if iter % 3 == 2:
+        _, name = attn_importances[attn_idx]
+        name_parts = name.split('.')
+        layer_idx = int(name_parts[0])
+        attn_module = model_utils.model.model.layers[layer_idx].self_attn
+        attn_generator = AttentionPruningTreeGenerator(cfg, attn_module)
+        for attn_tree in attn_generator.get_trees(model_utils):
+            attn_tree.prune()
+        attn_idx += 1
+    else:
+        _, name = mlp_importances[mlp_idx]
+        name_parts = name.split('.')
+        layer_idx = int(name_parts[0])
+        proj_type = name_parts[1]
+        mlp_module = model_utils.model.model.layers[layer_idx].mlp
+        mlp_proj = getattr(mlp_module, proj_type)
+        tree = TransformPruningTree(cfg, model_utils, root_module=mlp_proj)
+        tree.prune()
+        mlp_idx += 1
+
+IdentityPatcher(cfg, model_utils).patch()
+
+print(model_utils.model(model_utils.model.dummy_inputs['input_ids'].to('cuda')))
+print(model_utils.model)
+print(f"SPARSITY {model_utils.get_sparsity()}")
+
+pruned_model_utils = model_utils
+
+if cfg.EVALUATE:
+    assert cfg.PRUNED_MODEL_SAVE_DIR is not None
+    assert cfg.EVAL_RESULTS_PATH is not None
+    
+    evaluate_pruned(
+        cfg=cfg,
+        model_utils=pruned_model_utils
+    )
